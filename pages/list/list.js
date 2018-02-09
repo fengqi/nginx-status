7fd24baaf5187b6b93708e73fa65603f0f23cdcc
fengqi <lyf362345@gmail.com>
2018-02-09 13:55:51 +0800

    列表页完善

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,6 +1,5 @@
 // pages/list/list.js
 Page({
-
     /**
      * 页面的初始数据
      */
@@ -12,6 +11,7 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        console.log('onLoad');
         this.setData({
             server: wx.getStorageSync("server_list")
         })
@@ -21,48 +21,63 @@ Page({
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-
+        console.log('onReady');
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
+        console.log('onShow');
+        this.setData({
+            server: wx.getStorageSync("server_list")
+        })
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
     onHide: function () {
-
+        console.log('onHide');
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
     onUnload: function () {
-
+        console.log('onUnload');
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh: function () {
-
+        console.log('onPullDownRefresh');
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
-
+        console.log('onReachBottom');
     },
 
     /**
      * 用户点击右上角分享
      */
     onShareAppMessage: function () {
+        console.log('onShareAppMessage');
+    },
 
+    /**
+     * 点击变色
+     */
+    changeColor: function (res) {
+        var key, server = Object.keys(this.data.server);
+        this.data.server[res.currentTarget.id].changeColor = true;
+
+        this.setData({
+            server: this.data.server
+        })
     }
 })
